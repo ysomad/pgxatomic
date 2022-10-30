ebe5212977438b791b0dd855ce1a5bbfa754a8e4
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-10-31 04:47:38 +0700

    removed execTxFunc func

--- runner.go
+++ runner.go
@@ -27,13 +27,9 @@ func NewRunner(db txStarter, opts pgx.TxOptions) (*runner, error) {
 	}, nil
 }
 
+// Run executes txFunc within shared transaction.
 func (r *runner) Run(ctx context.Context, txFunc func(ctx context.Context) error) error {
-	return execTxFunc(ctx, r.db, r.opts, txFunc)
-}
-
-// execTxFunc executes txFunc withing shared transaction.
-func execTxFunc(ctx context.Context, db txStarter, opts pgx.TxOptions, txFunc func(ctx context.Context) error) error {
-	return pgx.BeginTxFunc(ctx, db, opts, func(tx pgx.Tx) error {
+	return pgx.BeginTxFunc(ctx, r.db, r.opts, func(tx pgx.Tx) error {
 		return txFunc(withTx(ctx, tx))
 	})
 }
