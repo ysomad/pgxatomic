87ed84da904c074b2a9e6be1a6e6b0585e80634e
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-11-01 03:46:26 +0700

    removed txStarter interface, passing pool into wrapper directly since its not possible to use with pgx.Conn in a clean way

--- wrapper.go
+++ wrapper.go
@@ -5,31 +5,28 @@ import (
 	"errors"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type txStarter interface {
-	BeginTx(context.Context, pgx.TxOptions) (pgx.Tx, error)
-}
-
 type wrapper struct {
-	db   txStarter
+	p    *pgxpool.Pool
 	opts pgx.TxOptions
 }
 
-func NewWrapper(db txStarter, opts pgx.TxOptions) (*wrapper, error) {
-	if db == nil {
-		return nil, errors.New("pgxatomic: db cannot be nil")
+func NewWrapper(p *pgxpool.Pool, opts pgx.TxOptions) (*wrapper, error) {
+	if p == nil {
+		return nil, errors.New("pgxatomic: pool cannot be nil")
 	}
 
 	return &wrapper{
-		db:   db,
+		p:    p,
 		opts: opts,
 	}, nil
 }
 
 // Wrap wraps txFunc in pgx.BeginTxFunc with injected pgx.Tx into context.
 func (w *wrapper) Wrap(ctx context.Context, txFunc func(ctx context.Context) error) error {
-	return pgx.BeginTxFunc(ctx, w.db, w.opts, func(tx pgx.Tx) error {
+	return pgx.BeginTxFunc(ctx, w.p, w.opts, func(tx pgx.Tx) error {
 		return txFunc(withTx(ctx, tx))
 	})
 }
