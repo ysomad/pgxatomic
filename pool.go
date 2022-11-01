6d91646f868a6c323461bafedde6dbf6fe4993cc
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-11-01 20:48:13 +0700

    using pool from field

--- pool.go
+++ pool.go
@@ -11,21 +11,21 @@ import (
 // Pool wraps pgxpool.Pool query methods with pgxatomic corresponding functions
 // which injects pgx.Tx into context.
 type Pool struct {
-	*pgxpool.Pool
+	p *pgxpool.Pool
 }
 
-func NewPool(p *pgxpool.Pool) *Pool {
-	return &Pool{p}
+func NewPool(p *pgxpool.Pool) Pool {
+	return Pool{p: p}
 }
 
 func (p *Pool) Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error) {
-	return Query(ctx, p, sql, args...)
+	return Query(ctx, p.p, sql, args...)
 }
 
 func (p *Pool) QueryRow(ctx context.Context, sql string, args ...any) pgx.Row {
-	return QueryRow(ctx, p, sql, args...)
+	return QueryRow(ctx, p.p, sql, args...)
 }
 
 func (p *Pool) Exec(ctx context.Context, sql string, args ...any) (pgconn.CommandTag, error) {
-	return Exec(ctx, p, sql, args...)
+	return Exec(ctx, p.p, sql, args...)
 }
